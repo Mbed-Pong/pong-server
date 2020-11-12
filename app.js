32edd4961418bec7fd0822e81636edbb6910fd51
Deniz <deniz.c.guler@gmail.com>
2020-11-12 01:24:40 -0500

    added game logic with update() and tickForward() functions

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
-// const hostname = '127.0.0.1';
 const port = process.env.PORT;
 const { GameState } = require('./game.js');
 const { findAvailableLobby } = require('./matchMaking.js');
 
-var MAX_LOBBIES = 5;
+const MAX_LOBBIES = 5;
+const TICK_TIME = 20;
 let lobbies = new Map();
 
 server.on('error', (err) => {
@@ -20,7 +20,7 @@ server.on('connect', () => {
 });
 
 server.on('close', () => {
-  numPlayers -= 1;
+  // numPlayers -= 1;
 });
 
 server.on('message', (msg, rinfo) => {
@@ -36,7 +36,7 @@ server.on('message', (msg, rinfo) => {
           } else {
             // create new lobby and hash
             let hash = "jaredyaegersflipflop";
-            lobbies.set(hash, { net: [{ addr: rinfo.address, port: rinfo.port }], gameState: new GameState(128, 128) });
+            lobbies.set(hash, { net: [{ addr: rinfo.address, port: rinfo.port }], gameState: new GameState(128, 128, 5) });
             server.send('eyyyyyy bal', rinfo.port, rinfo.address);
           }
         } else {
@@ -45,12 +45,15 @@ server.on('message', (msg, rinfo) => {
           lobby.net[1] = { addr: rinfo.address, port: rinfo.port };
           // send hash to players when ready
           if (lobby.numPlayers === 2) {
-            // start game and send gamestate to both players 
-
+            // start game and send gamestate to both players
+            // need to come up with a way to clear the interval
+            // lobby.handler = setInterval(() => {lobby.gameState.tickForward}, TICK_TIME);
           }
         }
         break;
       case 'move':
+        let lobby = lobbies.get(json.hash);
+        lobby.gameState.update(json.player, json.delta);
         break;
     }
   } catch (e) {
--- game.js
+++ game.js
@@ -1,34 +1,75 @@
 // help me god
 
+const clampNumber = (num, min, max) => (Math.max(Math.min(num, max), min));
+
 /**
  * creates a game state that can be broadcast to connected devices. 
  * @param {*} x horizontal dimension of the board 
  * @param {*} y vertical dimension of the board 
+ * @param {*} pointsToWin points to win game
  */
 
 class GameState {
-  constructor(x, y) {
-    this.playerOnePos = 63; // y / 2 - 1
-    this.playerTwoPos = 63;
-    this.ballDir = [1.0, 0];
-    this.ballPos = [];
+  #ballSpeed = .2;
+  #x;
+  #y;
+  #pointsToWin;
+
+  constructor(x, y, pointsToWin) {
+    this.#x = x;
+    this.#y = y;
+    this.#pointsToWin = pointsToWin || 5;
+    this.playerOnePos = y / 2 - 1; // y / 2 - 1
+    this.playerTwoPos = x / 2 - 1;
+    this.ballDir = [1, 0];
+    this.ballPos = [x / 2 - 1, y / 2 - 1];
     this.score = [0, 0];
+    this.isOver = 0; // 1 for player 1 victory 2 for player 2 victory 3 for other/disconnect
   };
 
-  get isOver() {
-    return this.score[0] >= 3 || this.score[1] >= 3;
+  #resetBall() {
+    this.ballPos = [this.#x / 2 - 1, this.#y / 2 - 1]
   }
+
+  // get isOver() {
+  //   return this.score[0] >= 3 || this.score[1] >= 3;
+  // }
 
   /**
    * 
    */ 
-  update(move) {
+  update(player, move) {
     // respond to player move
+    // probably want to change these to reflect the size of the paddles
+    if (player === 0) {
+      this.playerOnePos = clampNumber(this.playerOnePos + move, 0, this.#y - 1);
+    }
+    if (player === 1) {
+      this.playerTwoPos = clampNumber(this.playerTwoPos + move, 0, this.#y - 1);
+    }
   }
 
   tickForward () {
-    console.log("hello world")
-    // move the ball, score, etc.
+    // console.log("hello world")
+    // move the ball
+    this.ballPos = this.ballDir.map((component, index) => {
+      this.ballPos[index] + component * this.#ballSpeed;
+    })
+    // player 2 scores
+    if (ballPos[0] <= 0) {
+      this.score[1]++;
+      this.#resetBall();
+    }
+    // player 1 scores
+    if (ballPos[0] >= this.#x - 1) {
+      this.score[0]++;
+      this.#resetBall();
+    }
+    // check for winner
+    if (Math.max(this.score) >= this.#pointsToWin) {
+      this.isOver = this.score.indexOf(Math.max(this.score)) + 1;
+    }
+    console.log(JSON.stringify(this));
   };
 
 };
