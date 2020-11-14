f2b5cc4586b2441152e75407d44e5d7f050ad997
Deniz <deniz.c.guler@gmail.com>
2020-11-13 21:37:43 -0500

    server should send information about the game state each tick

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { findAvailableLobby } = require('./matchMaking.js');
 
 const MAX_LOBBIES = 5;
 const TICK_TIME = 20;
+
 let lobbies = new Map();
 
 server.on('error', (err) => {
@@ -14,13 +15,10 @@ server.on('error', (err) => {
 });
 
 server.on('connect', () => {
-  // numPlayers += 1;
-  // matchmaking logic
   console.log('something connected');
 });
 
 server.on('close', () => {
-  // numPlayers -= 1;
 });
 
 server.on('message', (msg, rinfo) => {
@@ -37,18 +35,24 @@ server.on('message', (msg, rinfo) => {
             // create new lobby and hash
             let hash = "jaredyaegersflipflop";
             lobbies.set(hash, { net: [{ addr: rinfo.address, port: rinfo.port }], gameState: new GameState(128, 128, 5) });
-            server.send(JSON.stringify(lobbies.get(hash).gameState), rinfo.port, rinfo.address);
+            server.send(JSON.stringify({ type: 'connected', player: 0, hash: hash }), rinfo.port, rinfo.address);
           }
         } else {
           let lobby = lobbies.get(found);
           lobby.numPlayers++;
           lobby.net[1] = { addr: rinfo.address, port: rinfo.port };
           // send hash to players when ready
+          server.send(JSON.stringify({ type: 'connected', player: 1, hash: found }), rinfo.port, rinfo.address);
           if (lobby.numPlayers === 2) {
             // start game and send gamestate to both players
             // need to come up with a way to clear the interval
-            lobby.handle = setInterval(() => {lobby.gameState.tickForward}, TICK_TIME);
-            lobby.gameState.onEnd = () => {clearInterval(lobby.handle)}
+            lobby.gameState.onTickForward = () => {
+              lobby.net.map((netinfo) => {
+                server.send(JSON.stringify(lobby.gameState), netinfo.port, netinfo.addr);
+              })
+            }
+            lobby.ticker = setInterval(() => { lobby.gameState.tickForward }, TICK_TIME);
+            lobby.gameState.onEnd = () => { clearInterval(lobby.ticker) };
           }
         }
         break;
@@ -66,9 +70,5 @@ server.on('listening', () => {
   const address = server.address();
   console.log(`UDP Socket running on: ${address.address}:${port}`);
 })
-
-// TODO: rename TICK_TIME to PIZZA_TIME
-// const handler = setInterval(() => { }, TICK_TIME)
-// clearInterval(handler)
 
 server.bind(port);
--- game.js
+++ game.js
@@ -3,25 +3,28 @@
 const clampNumber = (num, min, max) => (Math.max(Math.min(num, max), min));
 
 /**
- * creates a game state that can be broadcast to connected devices. 
- * @param {*} x horizontal dimension of the board 
- * @param {*} y vertical dimension of the board 
- * @param {*} pointsToWin points to win game
+ * Game state class that handles the game state
  */
-
 class GameState {
   #ballSpeed = .2;
+  #ballDir;
   #x;
   #y;
   #pointsToWin;
 
+  /**
+   * creates a game state that can be broadcast to connected devices. 
+   * @param {number} x horizontal dimension of the board 
+   * @param {number} y vertical dimension of the board 
+   * @param {number} pointsToWin points to win game
+   */
   constructor(x, y, pointsToWin) {
     this.#x = x;
     this.#y = y;
     this.#pointsToWin = pointsToWin || 5;
     this.playerOnePos = y / 2 - 1; // y / 2 - 1
     this.playerTwoPos = x / 2 - 1;
-    this.ballDir = [1, 0];
+    this.#ballDir = [1, 0];
     this.ballPos = [x / 2 - 1, y / 2 - 1];
     this.score = [0, 0];
     this.isOver = 0; // 1 for player 1 victory 2 for player 2 victory 3 for other/disconnect
@@ -36,19 +39,28 @@ class GameState {
   // }
 
   /**
+   * called when game ends
    * @param {() => void} onEnd
    */
   set onEnd(onEnd) {
     this.onEnd = onEnd;
   }
 
-  end() {
+  /**
+   * called at the end of a tick cycle
+   * @param {() => void} onTickForward
+   */
+  set onTickForward(onTickForward) {
+    this.onTickForward = onTickForward;
+  }
+
+  #end() {
     this.onEnd();
   }
 
   /**
    * 
-   */ 
+   */
   update(player, move) {
     // respond to player move
     // probably want to change these to reflect the size of the paddles
@@ -60,10 +72,10 @@ class GameState {
     }
   }
 
-  tickForward () {
+  tickForward() {
     // console.log("hello world")
     // move the ball
-    this.ballPos = this.ballDir.map((component, index) => {
+    this.ballPos = this.#ballDir.map((component, index) => {
       this.ballPos[index] + component * this.#ballSpeed;
     })
     // player 2 scores
@@ -79,8 +91,12 @@ class GameState {
     // check for winner
     if (Math.max(this.score) >= this.#pointsToWin) {
       this.isOver = this.score.indexOf(Math.max(this.score)) + 1;
-      this.end();
     }
+    // check for game over
+    if (isOver) {
+      this.#end();
+    }
+    this.onTickForward();
     console.log(JSON.stringify(this));
   };
 
@@ -88,7 +104,7 @@ class GameState {
 
 // export default { GameState }
 
-module.exports.GameState = GameState; 
+module.exports.GameState = GameState;
 // JSON.stringify(gameState)
 
 
